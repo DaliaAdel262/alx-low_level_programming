d6c6d8e3be687c50fb65d3d23bbf8bdc1d74d7c8
Dalia Adel <daliaadel262@gmail.com>
2023-02-21 14:44:53 -0800

    task3

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_alphabet-x10 - prints alphabet in lowercase 10 times
+ */
+
+void print_alphabet_x10(void)
+{
+	char alpha;
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (alpha = 'a'; alpha <= 'z'; alpha++)
+		{
+			_putchar(alpha);
+		}
+
+		_putchar('\n');
+	}
+}
