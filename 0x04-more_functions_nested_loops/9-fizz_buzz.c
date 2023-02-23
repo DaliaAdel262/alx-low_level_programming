c077d76888bd6412b9dfccedc4d2fe750dc8e73c
Dalia Adel <daliaadel262@gmail.com>
2023-02-23 14:59:53 -0800

    task9

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+
+/**
+ * main - print numbers from 1 to 100 except multiples of three and five
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	int num;
+
+	for (num = 1; num <= 100; num++)
+	{
+		if ((num % 3) == 0)
+		{
+			if ((num % 5) == 0)
+			{
+				printf("FizzBuzz ");
+			}
+			else
+			{
+				printf("Fizz ");
+			}
+		}
+		else if ((num % 5) == 0)
+		{
+			printf("Buzz ");
+		}
+		else
+		{
+			printf("%d ", num);
+		}
+	}
+	printf("\n");
+	return (0);
+}
+
