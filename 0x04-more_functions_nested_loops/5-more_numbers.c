144283cab90ed4bda0b5aeea2f1885f9ae968f03
Dalia Adel <daliaadel262@gmail.com>
2023-02-23 13:47:08 -0800

    task5

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * more_numbers - prints numbers from 0 to 14, 10 times
+ * Return: numbers from 0 to 14, 10 times
+ */
+
+void more_numbers(void)
+{
+	int i, c;
+
+	for (i = 0; i <= 10; i++)
+	{
+		for (c = 0; c <= 14; c++)
+		{
+			if (c > 9)
+			{
+				_putchar((c / 10) + '0');
+			}
+			_putchar((c % 10) + '0');
+		}
+		_putchar('\n');
+	}
+}
