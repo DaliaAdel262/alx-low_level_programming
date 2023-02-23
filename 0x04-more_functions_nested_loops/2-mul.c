098da0f01d2ae6406e841d3f79d880b11803c6cf
Dalia Adel <daliaadel262@gmail.com>
2023-02-23 12:41:59 -0800

    task2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * mul - multiplies two integers
+ * @a: first integer to be computed
+ * @b: second integer to be computed
+ * Return: factor of a and b
+ */
+
+int mul(int a, int b)
+{
+	int factor;
+
+	factor = a * b;
+	return (factor);
+}
