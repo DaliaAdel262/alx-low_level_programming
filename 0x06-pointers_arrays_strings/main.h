1ddf9ef2a3ab8446f7d74a0d8a562634ab7115f6
Dalia Adel <daliaadel262@gmail.com>
2023-03-03 14:09:14 -0800

    task8

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -16,4 +16,11 @@ char *leet(char *n);
 
 int _putchar(char c);
 
-char *rot13(char *s)
+char *rot13(char *a);
+
+void print_number(int n);
+
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
+
+void print_buffer(char *b, int size);
+
