8bd8cd27c5dfe6c8667e5f233cca735d1f3378d0
Dalia Adel <daliaadel262@gmail.com>
2023-03-24 14:47:02 -0700

    task2

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -3,7 +3,7 @@
 /**
  *  _strlen_recursion - returns length of a string
  * @s: string to be counted
- * Return: length of string or 
+ * Return: length of string or
  */
 
 int _strlen_recursion(char *s)
@@ -13,7 +13,7 @@ int _strlen_recursion(char *s)
 	if (*s != '\0')
 	{
 		count++;
-		count += _strlen_recursion(s + 1);	
+		count += _strlen_recursion(s + 1);
 	}
 	return (count);
 }
